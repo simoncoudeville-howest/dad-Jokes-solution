ebf603b230d7ccb507e1789d3a4681761f00d8a4
MLoth <MLoth@users.noreply.github.com>
2023-10-09 12:59:22 +0200

    feat: show basic joke

--- script/app.js
+++ script/app.js
@@ -1,5 +1,22 @@
-const init = function () {
-    console.log('script loaded');
+const renderJoke = (joke) => {
+  console.log(joke)
+  const jokeElement = document.querySelector('.js-joke-content')
+  jokeElement.innerHTML = joke.joke
 }
 
-init();
+const getJoke = () => {
+  return fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then((r) => r.json())
+    .finally(() => {})
+}
+
+const init = async function () {
+  const joke = await getJoke()
+  renderJoke(joke)
+}
+
+init()
